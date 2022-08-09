95cb0ac0c1286e22feb9da5317ba08202b54e710
Jerry L <hyde545@gmail.com>
2022-08-09 10:44:51 +0800

    Add date format transfer

--- CSV_StrDateToNum.py
+++ CSV_StrDateToNum.py
@@ -2,8 +2,16 @@ import pandas as pd
 
 df = pd.read_csv('./PTATMonitor_05-08-2022_15H-23-36S399.csv')
 
-df['timestamp'] = df['timestamp'].str.replace(":", "")
+for columns in df:
+    if columns == 'timestamp':
+        df[columns] = df[columns].str.replace(":", "")
+    if columns == 'Date':
+        df[columns] = pd.to_datetime(df[columns], dayfirst=True)
+        df[columns] = df[columns].astype(str)
+        df[columns] = df[columns].str.replace("-", "")
+        # print(df[columns])
+    print(df[columns].dtypes)
 
-print(df['timestamp'])
+# print(df['timestamp'])
 
 df.to_csv('./PTATMonitor_05-08-2022_15H-23-36S399_parse.csv', index=False)
