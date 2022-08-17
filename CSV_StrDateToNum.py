24654321bc50fecd50c3ca041597bd130c501410
Jerry L <hyde545@gmail.com>
2022-08-17 09:49:17 +0800

    Read all csv files in folder

--- CSV_StrDateToNum.py
+++ CSV_StrDateToNum.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import os
+import glob
 
-df = pd.read_csv('./PTATMonitor_05-08-2022_15H-23-36S399.csv')
+path = os.getcwd()
+csv_files = glob.glob(os.path.join(path, "*.csv"))
 
-for columns in df:
-    if columns == 'timestamp':
-        df[columns] = df[columns].str.replace(":", "").astype(int)
-    if columns == 'Date':
-        df[columns] = pd.to_datetime(df[columns], dayfirst=True)
-        df[columns] = df[columns].astype(str)
-        df[columns] = df[columns].str.replace("-", "").astype(int)
-    if df[columns].dtypes == object:
-        df[columns] = df[columns].eq('Yes').mul(1)
-        # df[columns] = df[columns].map(dict('Yes'=1, 'No'=0), na_action='ignore')
+for file in csv_files:
+    df = pd.read_csv(file)
 
-df.to_csv('./PTATMonitor_05-08-2022_15H-23-36S399_parse.csv', index=False)
+    df.columns = df.columns.str.replace('Time', 'timestamp')
+    
+    for columns in df:
+        if columns == 'timestamp':
+            df[columns] = df[columns].str.replace(":", "").astype(int)
+        if columns == 'Date':
+            df[columns] = pd.to_datetime(df[columns], dayfirst=True)
+            df[columns] = df[columns].astype(str)
+            df[columns] = df[columns].str.replace("-", "").astype(int)
+        if df[columns].dtypes == object:
+            df[columns] = df[columns].eq('Yes').mul(1)
+            # df[columns] = df[columns].map(dict('Yes'=1, 'No'=0), na_action='ignore')
+
+    df.to_csv(file + "_parse", index=False)
