b234f5d9f4e1bc480b77331f70a69c6b21ffa82d
Jerry L <hyde545@gmail.com>
2022-08-17 15:43:52 +0800

    1. Drop date columns for now. 2. rename csv file after data parsing.

--- CSV_StrDateToNum.py
+++ CSV_StrDateToNum.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import os
 import glob
+import re
 
 path = os.getcwd()
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
-for file in csv_files:
+for i, file in enumerate(csv_files):
     df = pd.read_csv(file)
 
     df.columns = df.columns.str.replace('Time', 'timestamp')
-    
+    df.drop('Date', axis=1, inplace=True) #currently doesn't need date columns
+
     for columns in df:
         if columns == 'timestamp':
             df[columns] = df[columns].str.replace(":", "").astype(int)
@@ -21,4 +23,15 @@ for file in csv_files:
             df[columns] = df[columns].eq('Yes').mul(1)
             # df[columns] = df[columns].map(dict('Yes'=1, 'No'=0), na_action='ignore')
 
-    df.to_csv(file + "_parse", index=False)
+    # classify_name = file.split('\\')
+    classify_name = re.split(r'( |/|\\)', file)
+    list_len = len(classify_name)
+    print(classify_name[list_len-1])
+
+    classify_name[list_len-1] = classify_name[list_len-1].split('_')[0] + "_" + str(i)
+    print(classify_name[list_len-1])
+    file = ''
+    for a in classify_name:
+        file += a 
+
+    df.to_csv(file + ".csv", index=False)
